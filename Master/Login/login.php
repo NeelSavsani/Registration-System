803543c75279f39be92d3485303a1c5dbe973d6d
MasterEagle <81796957+NeelSavsani@users.noreply.github.com>
2024-10-12 02:30:41 +0530

    Update login.php

--- Master/Login/login.php
+++ Master/Login/login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
     <script src="https://kit.fontawesome.com/e05d24f6c7.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="Login.css">
+    <link rel="stylesheet" href="login.css">
 </head>
 
 <body>
