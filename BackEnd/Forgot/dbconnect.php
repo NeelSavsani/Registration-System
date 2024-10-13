32611329e09d74c05d86a58c26e4db220dd7fd8a
MasterEagle <81796957+NeelSavsani@users.noreply.github.com>
2024-10-13 18:21:05 +0530

    Update dbconnect.php

--- BackEnd/Forgot/dbconnect.php
+++ BackEnd/Forgot/dbconnect.php
@@ -3,14 +3,29 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dadtabase = "project";
+$database = "project";
 $table ="login_credentials";
 
-$conn = mysqli_connect($servername, $username, $password, $dadtabase);
+$conn = mysqli_connect($servername, $username, $password);
 
 if(!$conn)
 {
     die("Error".mysqli_connect_error());
+}
+else
+{
+    $sql = "CREATE DATABASE IF NOT EXISTS `$database`";
+    $result = mysqli_query($conn, $sql);
+    if(!$result)
+    {
+        echo "Creation of Database of Failed!<br>";
+    }
+    $sql = "CREATE TABLE IF NOT EXISTS `$database`.`$table` (`Sr. No.` INT(10) NOT NULL AUTO_INCREMENT , `Email Id` VARCHAR(30) NOT NULL , `Password` VARCHAR(30) NOT NULL , `Date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`Sr. No.`)) ENGINE = InnoDB";
+    $result = mysqli_query($conn, $sql);
+    if(!$result)
+    {
+        echo "Creation of table was failed!<br>";
+    }
 }
 
 ?>
